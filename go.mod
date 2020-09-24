b146bda438b0c75e7955b5f165343d101c5aed18
hirs_2004 <hirs_2004@mail.ru>
2020-09-24 22:55:09 +0500

    added types Status

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/hirs-500/bank
+module github.com/hirs-500/bank/v2
 
 go 1.15
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -2,13 +2,24 @@
 package types
 // Money представляет собой денежную сумму в минимальных единицах (дирамы, рубль, центы, и т.д ).
 type Money int64
+
 // Category представляет собой котегорию, который быль совершён платёж (авто, аптека, ресторан, т.д.).
 type Category string
+
+// Status представлет собой статус платжа.
+type Status string
+// Предопределённый статус платежей.
+const(
+	StatusOk Status = "OK"
+	StatusFail Status = "FAIL"
+	StatusInProgress Status = "INPROGRESS"
+)
 
 // Payment представляет информацию о платеже.
 type Payment struct {
 	ID     int
 	Amount  Money//использовали Money 
 	Category  Category
-	
-} 
+	Status    Status
+}
+ 
